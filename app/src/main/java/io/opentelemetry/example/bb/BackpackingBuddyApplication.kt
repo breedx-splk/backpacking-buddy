e50cce97df8168561fc96a1ebc3823eaf2df9481
Jason Plumb <jplumb@splunk.com>
2024-03-12 16:53:47 -0700

    wire up otel rum and click handlers to generate spans!

--- app/src/main/java/io/opentelemetry/example/bb/BackpackingBuddyApplication.kt
+++ app/src/main/java/io/opentelemetry/example/bb/BackpackingBuddyApplication.kt
@@ -6,6 +6,7 @@ import io.opentelemetry.android.OpenTelemetryRum
 import io.opentelemetry.android.OpenTelemetryRumBuilder
 import io.opentelemetry.android.config.OtelRumConfig
 import io.opentelemetry.android.features.diskbuffering.DiskBufferingConfiguration
+import io.opentelemetry.api.trace.Tracer
 
 class BackpackingBuddyApplication : Application() {
 
@@ -20,10 +21,18 @@ class BackpackingBuddyApplication : Application() {
             .setDiskBufferingConfiguration(diskBufferingConfig)
         val otelRumBuilder: OpenTelemetryRumBuilder = OpenTelemetryRum.builder(this, config)
         try {
-            val rum = otelRumBuilder.build()
-            Log.d("bb", "RUM session started: " + rum.rumSessionId)
+            rum = otelRumBuilder.build()
+
+            Log.d("bb", "RUM session started: " + rum!!.rumSessionId)
         } catch (e: Exception) {
             Log.e("bb", "Oh no!", e)
+        }
+    }
+
+    companion object {
+        var rum: OpenTelemetryRum? = null
+        fun tracer(name: String): Tracer? {
+            return rum?.openTelemetry?.tracerProvider?.get("")
         }
     }
 }
--- app/src/main/java/io/opentelemetry/example/bb/MainActivity.kt
+++ app/src/main/java/io/opentelemetry/example/bb/MainActivity.kt
@@ -7,20 +7,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import io.opentelemetry.example.bb.ui.theme.BackpackingBuddyTheme
 
 class MainActivity : ComponentActivity() {
@@ -53,22 +46,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-}
-
-@Preview
-@Composable
-fun Disposition() {
-    Row(horizontalArrangement = Arrangement.SpaceEvenly) {
-        TextButton(onClick = {}) {
-            Text(text = "\uD83D\uDE0A", fontSize = 75.sp)
-        }
-        TextButton(onClick = {}) {
-            Text(text = "\uD83D\uDE10", fontSize = 75.sp)
-        }
-        TextButton(onClick = {}) {
-            Text(text = "☹\uFE0F", fontSize = 75.sp)
-        }
-    }
 }
 
 
