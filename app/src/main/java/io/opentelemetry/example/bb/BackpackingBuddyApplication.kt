0aabe000a6a4660b899919a92c62b9ec8f0f841a
Jason Plumb <jplumb@splunk.com>
2024-03-17 05:41:05 -0700

    talk to jaeger and use insecure otlp http

--- app/src/main/java/io/opentelemetry/example/bb/BackpackingBuddyApplication.kt
+++ app/src/main/java/io/opentelemetry/example/bb/BackpackingBuddyApplication.kt
@@ -1,16 +1,20 @@
 package io.opentelemetry.example.bb
 
+import android.annotation.SuppressLint
 import android.app.Application
 import android.util.Log
 import io.opentelemetry.android.OpenTelemetryRum
 import io.opentelemetry.android.OpenTelemetryRumBuilder
 import io.opentelemetry.android.config.OtelRumConfig
 import io.opentelemetry.android.features.diskbuffering.DiskBufferingConfiguration
+import io.opentelemetry.android.internal.services.ServiceManager
 import io.opentelemetry.api.trace.Tracer
+import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter
 
 class BackpackingBuddyApplication : Application() {
 
+    @SuppressLint("RestrictedApi")
     override fun onCreate() {
         super.onCreate()
         Log.i("bb", "Initializing the opentelemetry-android-agent")
@@ -22,13 +26,16 @@ class BackpackingBuddyApplication : Application() {
             .setDiskBufferingConfiguration(diskBufferingConfig)
         val otelRumBuilder: OpenTelemetryRumBuilder = OpenTelemetryRum.builder(this, config)
             .addSpanExporterCustomizer {
-                OtlpGrpcSpanExporter.builder()
-                    .setEndpoint("https://some.host")
+                OtlpHttpSpanExporter.builder()
+                    // I don't yet know of a good way to configure the local docker address...
+                    // The emulator is a different VM and "localhost" doesn't work.
+//                    .setEndpoint("http://localhost:4318/v1/traces")
+                    .setEndpoint("http://192.168.66.1:4318/v1/traces")
                     .build()
             }
         try {
             rum = otelRumBuilder.build()
-
+            ServiceManager.get().start();
             Log.d("bb", "RUM session started: " + rum!!.rumSessionId)
         } catch (e: Exception) {
             Log.e("bb", "Oh no!", e)
